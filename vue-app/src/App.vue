3da0909bbada3f6a2d2bbe58cc4c6460b6e51bc8
kasprzyk <piotkas1@gmail.com>
2019-11-15 15:14:45 +0100

    Navigation-drawer and first component added

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,56 +1,45 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+  <v-app id="inspire" :dark="themeMode == 'dark'">
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list dense>
+        <v-list-group value="true">
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-title>Konfiguracja</v-list-tile-title>
+            </v-list-tile>
+          </template>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-content>
-      <HelloWorld />
+      <v-container fluid>
+        <v-row justify="center" align="center">
+          <v-col>
+            <StartView v-if="view == 'home'" />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import StartView from "./components/HelloWorld";
 
 export default {
-  name: "App",
-
   components: {
-    HelloWorld
+    StartView
   },
-
   data: () => ({
-    //
-  })
+    drawer: null,
+    view: "home"
+  }),
+  created() {
+    console.log(this);
+  }
 };
 </script>
