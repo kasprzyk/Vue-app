7e78ae6f107d87e83ca077c02af04c6d79770b52
kasprzyk <piotkas1@gmail.com>
2020-01-23 16:41:18 +0100

    First service added

---
+++ vue-app/src/server/api.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("TEST");
+});
+
+router.post("/getDictionary", (req, res) => {
+  var params = JSON.parse(req.body.params);
+  var { dictionaryName } = params;
+
+  var data = require(`./data/${dictionaryName}.json`);
+
+  setTimeout(() => {
+    res.status("200").json(data);
+    res.end();
+  }, 200);
+});
