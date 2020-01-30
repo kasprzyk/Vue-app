bd759f71a6adf922fcfee06598240c51c2032d5c
kasprzyk <piotkas1@gmail.com>
2020-01-30 16:11:08 +0100

    added configure.js

---
+++ vue-app/src/server/configure.js
@@ -0,0 +1,9 @@
+const bodyParser = require("body-parser");
+const api = require("./api");
+const webdav = require("./webdav");
+module.exports = app => {
+  app.use(bodyParser.json({ limit: "50mb" }));
+  app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+  app.use("/api", api);
+  app.use("/mum", webdav);
+};
